e418a34ebe71e1dbdb0c87820a199e3d4498bd1b
s026324 <ss026324@gmail.com>
2018-10-24 17:29:37 +0300

    10.24

--- app/src/main/java/com/example/wrongparking/Redaktorius.java
+++ app/src/main/java/com/example/wrongparking/Redaktorius.java
@@ -2,7 +2,6 @@ package com.example.wrongparking;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -10,34 +9,116 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class Redaktorius extends AppCompatActivity {
 
-    private static final String TAG = "ViewData";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+
+
+
+
 
     private FirebaseDatabase mDatabase;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference myRef;
-    private String userID;
-    ArrayList<String> list;
-    ArrayAdapter<String> adapter;
-    Redatvaizdavimas redList;
+
+
 
     private ListView mListView;
+    private ArrayList<Upload> mUploads = new ArrayList<>();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_redaktorius);
+        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.recycler_list);
+
+        myRef = FirebaseDatabase.getInstance().getReference(DATABASE_PATH_UPLOADS);
+
+        mListView = (ListView) findViewById(R.id.redList);
+
+        final ArrayAdapter<Upload> arrayAdapter = new ArrayAdapter<Upload>(this, android.R.layout.simple_list_item_1, mUploads);
+
+/*        RecyclerView recyclerList = (RecyclerView) findViewById(R.id.recycler);
+        recyclerList.setLayoutManager(new LinearLayoutManager(this));
+        String[] ddd = {"bambo"};
+        recyclerList.setAdapter(new Recycler(ddd));*/
+/*
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.e("Count " ,""+snapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+                    Log.d("snapshaotValue", postSnapshot.toString());
+                    Upload post = postSnapshot.getValue(Upload.class);
+                    mUploads.add(post);
+                    Log.e("Get Data", post.getName());
+                    mListView.setAdapter(arrayAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("The read failed: " ,databaseError.getMessage());
+            }
+        });*/
+
+
+
+
+//        myRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                String value = dataSnapshot.getValue(String.class);
+//                mName.add(value);
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//        Query myTopPostsQuery = myRef.child("uploads").child("wot")
+//                .orderByChild("valstnum");
+//
+//        myTopPostsQuery.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+//                    Log.d("elementai",postSnapshot.toString());
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+
     }
 
 
@@ -47,31 +128,6 @@ public class Redaktorius extends AppCompatActivity {
             startActivity(new Intent(Redaktorius.this, MainActivity.class));
             Toast.makeText(Redaktorius.this, "Atsijunget", Toast.LENGTH_LONG).show();
         }
-        redList = new Redatvaizdavimas();
-        mListView = (ListView) findViewById(R.id.redList);
-
-        mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance();
-        myRef = mDatabase.getReference("uploads");
-        list = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this, R.layout.info_upload, R.id.uploadInfo, list);
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot ds : dataSnapshot.getChildren())
-                {
-                    redList = ds.getValue(Redatvaizdavimas.class);
-                    list.add(redList.getName().toString());
-
-                }
-                mListView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
         }
 
